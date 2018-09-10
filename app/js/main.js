d9589ae08d4d5eb12ed00a3b3ea7453a085020ef
annaklzn <klyaznika.a@outlook.com>
2018-09-10 15:01:26 +0300

    hover&animate+-

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
-$(function(){
-//CODE...
-
-});
+$(document).ready(function() {        
+        $(".nav_item_hover").hover(function(){
+           $(this).stop(true).fadeTo("fast",1); // This sets the opacity to 100%     on hover
+        },function(){
+            $(this).stop(true).fadeTo("fast", 0); // This sets the opacity back to 60% on mouseout
+        }); });
 
